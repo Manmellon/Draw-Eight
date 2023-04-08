be69887b7acea03800c80feb2497f0a5e40f9a62
Manmellon <24492231+Manmellon@users.noreply.github.com>
2023-04-08 14:02:55 +0300

    Check button and result label

---
+++ Assets/Scripts/Checker.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Checker : MonoBehaviour
+{
+    [SerializeField] private LineRenderer _lineRenderer;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void Check()
+    {
+        
+    }
+}
