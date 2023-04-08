afcfe8ebe4e9a2eefff9bb07aec20c935f62d90d
Manmellon <24492231+Manmellon@users.noreply.github.com>
2023-04-08 14:14:58 +0300

    Check if drawing is more horizontal or vertical

--- Assets/Scripts/Checker.cs
+++ Assets/Scripts/Checker.cs
@@ -1,10 +1,12 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class Checker : MonoBehaviour
 {
     [SerializeField] private LineRenderer _lineRenderer;
+    [SerializeField] private TextMeshProUGUI resultText;
 
     // Start is called before the first frame update
     void Start()
@@ -20,6 +22,41 @@ public class Checker : MonoBehaviour
 
     public void Check()
     {
-        
+        Vector3[] positions = new Vector3[_lineRenderer.positionCount];
+        _lineRenderer.GetPositions(positions);
+
+        //Find min and max x,y coordinates
+        Vector2 minPos = positions[0];
+        Vector2 maxPos = positions[0];
+
+        foreach (Vector3 pos in positions)
+        {
+            if (pos.x < minPos.x)
+            {
+                minPos.x = pos.x;
+            }
+            else if (pos.x > maxPos.x)
+            {
+                maxPos.x = pos.x;
+            }
+
+            if (pos.y < minPos.y)
+            {
+                minPos.y = pos.y;
+            }
+            else if (pos.y > maxPos.y)
+            {
+                maxPos.y = pos.y;
+            }
+        }
+
+        if (maxPos.x - minPos.x > maxPos.y - minPos.y)
+        {
+            resultText.text = "Horizontal";
+        }
+        else
+        {
+            resultText.text = "Vertical";
+        }
     }
 }
--- Assets/Scripts/Drawer.cs
+++ Assets/Scripts/Drawer.cs
@@ -2,11 +2,13 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.EventSystems;
+using TMPro;
 
 public class Drawer : MonoBehaviour
 {
     [SerializeField] private Camera _camera;
     [SerializeField] private LineRenderer _lineRenderer;
+    [SerializeField] private TextMeshProUGUI resultText;
 
     // Start is called before the first frame update
     void Start()
@@ -28,5 +30,6 @@ public class Drawer : MonoBehaviour
     public void Clear()
     {
         _lineRenderer.positionCount = 0;
+        resultText.text = "";
     }
 }
