11314d552928062e5280125676622e986a7ef2c7
Manmellon <24492231+Manmellon@users.noreply.github.com>
2023-04-08 13:46:06 +0300

    Create Drawer and clear button

---
+++ Assets/Scripts/Drawer.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class Drawer : MonoBehaviour
+{
+    [SerializeField] private Camera _camera;
+    [SerializeField] private LineRenderer _lineRenderer;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _camera = Camera.main;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButton(0) && !EventSystem.current.IsPointerOverGameObject())
+        {
+            Vector3 mousePos = _camera.ScreenToWorldPoint(Input.mousePosition);
+            _lineRenderer.positionCount += 1;
+            _lineRenderer.SetPosition(_lineRenderer.positionCount - 1, mousePos);
+        }
+    }
+
+    public void Clear()
+    {
+        _lineRenderer.positionCount = 0;
+    }
+}
