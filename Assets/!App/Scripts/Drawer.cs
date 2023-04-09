df7a6c4b47fb5eabd970e45c90750558ec96f3eb
Manmellon <24492231+Manmellon@users.noreply.github.com>
2023-04-09 17:17:45 +0300

    Allow draw several lines

--- Assets/!App/Scripts/Drawer.cs
+++ Assets/!App/Scripts/Drawer.cs
@@ -7,7 +7,8 @@ using TMPro;
 public class Drawer : MonoBehaviour
 {
     [SerializeField] private Camera _camera;
-    [SerializeField] private LineRenderer _lineRenderer;
+    [SerializeField] private Transform _linesParent;
+    [SerializeField] private LineRenderer _lineRendererPrefab;
     [SerializeField] private TextMeshProUGUI resultText;
 
     // Start is called before the first frame update
@@ -21,15 +22,27 @@ public class Drawer : MonoBehaviour
     {
         if (Input.GetMouseButton(0) && !EventSystem.current.IsPointerOverGameObject())
         {
+            if (Input.GetMouseButtonDown(0))
+            {
+                Instantiate(_lineRendererPrefab, _linesParent);
+            }
+
             Vector3 mousePos = _camera.ScreenToWorldPoint(Input.mousePosition);
-            _lineRenderer.positionCount += 1;
-            _lineRenderer.SetPosition(_lineRenderer.positionCount - 1, mousePos);
+
+            var lineRenderers = _linesParent.GetComponentsInChildren<LineRenderer>();
+            lineRenderers[lineRenderers.Length - 1].positionCount += 1;
+            lineRenderers[lineRenderers.Length - 1].SetPosition(lineRenderers[lineRenderers.Length - 1].positionCount - 1, mousePos);
         }
     }
 
     public void Clear()
     {
-        _lineRenderer.positionCount = 0;
+        foreach (var lineRenderer in _linesParent.GetComponentsInChildren<LineRenderer>())
+        {
+            Destroy(lineRenderer.gameObject);
+            //_lineRenderer.positionCount = 0;
+        }
+        
         resultText.text = "";
     }
 }
